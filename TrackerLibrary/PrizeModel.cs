ebf7bb1ab123a248c3e4d9329cfd09f9abec1189
math-reis <math.reis.dev@gmail.com>
2021-10-13 12:45:28 -0300

    final classes

---
+++ TrackerLibrary/PrizeModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary
+{
+    public class PrizeModel
+    {
+        public Int16 PlaceNumber { get; set; }
+        public String PlaceName { get; set; }
+        public Decimal PrizeAmount { get; set; }
+        public Double PrizePercentage { get; set; }
+    }
+}
